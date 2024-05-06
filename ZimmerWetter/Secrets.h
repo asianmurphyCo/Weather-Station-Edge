fe929ff82baddd42895a7d3a9e18816d8028c186
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-05-06 13:18:14 +0700

    add weather station config info

--- ZimmerWetter/Secrets.h
+++ ZimmerWetter/Secrets.h
@@ -2,18 +2,18 @@
 #define SECRET_DATA
 
 /// @brief MQTT Broker
-#define MQTT_BROKER "123.123.123.123"
+#define MQTT_BROKER "172.16.6.169"
 
 /// @brief MQTT Broker Port
 #define MQTT_PORT 1883
 
 /// @brief MQTT Topic
-#define MQTT_TOPIC "#"
+#define MQTT_TOPIC "swinburne/hcmc_a35"
 
 /// @brief WiFi name (SSID)
-#define WIFI_NAME "Airport WiFi!! (Totally not Suspicious!!)"
+#define WIFI_NAME "Swinburne Students"
 
 /// @brief WiFi password
-#define WIFI_PASS "joemama"
+#define WIFI_PASS "Swinburne@a35"
 
 #endif // MARK: SECRET_DATA
